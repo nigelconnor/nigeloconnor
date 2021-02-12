9d1a993a52da4fc2d4a75f4eee1e61ce220e6b19
nigelconnor <nigel.oconnor@stitcherads.com>
2021-02-12 15:58:10 +0000

    contact fix

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -10,54 +10,10 @@ export class Contact extends Component {
               <div className="row">
                 <div className="section-title">
                   <h2>Get In Touch</h2>
-                  <p>
-                    Please fill out the form below to send us an email and we
-                    will get back to you as soon as possible.
-                  </p>
                 </div>
-                <form name="sentMessage" id="contactForm" noValidate>
-                  <div className="row">
-                    <div className="col-md-6">
-                      <div className="form-group">
-                        <input
-                          type="text"
-                          id="name"
-                          className="form-control"
-                          placeholder="Name"
-                          required="required"
-                        />
-                        <p className="help-block text-danger"></p>
-                      </div>
-                    </div>
-                    <div className="col-md-6">
-                      <div className="form-group">
-                        <input
-                          type="email"
-                          id="email"
-                          className="form-control"
-                          placeholder="Email"
-                          required="required"
-                        />
-                        <p className="help-block text-danger"></p>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="form-group">
-                    <textarea
-                      name="message"
-                      id="message"
-                      className="form-control"
-                      rows="4"
-                      placeholder="Message"
-                      required
-                    ></textarea>
-                    <p className="help-block text-danger"></p>
-                  </div>
-                  <div id="success"></div>
-                  <button type="submit" className="btn btn-custom btn-lg">
-                    Send Message
-                  </button>
-                </form>
+                <div>
+                  <img src="img/gmaps.png" width="703" height="500" alt=""/>
+                </div>
               </div>
             </div>
             <div className="col-md-3 col-md-offset-1 contact-info">
